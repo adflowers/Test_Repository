6585531702147567bb359ed1714087ccc4174fb5
Unknown <adflowers@gmail.com>
2017-11-16 19:05:54 -0500

    Beta Build 03
    Added Skeleton Function:
    Print Money

--- Amazing_Code.cpp
+++ Amazing_Code.cpp
@@ -16,4 +16,11 @@ int main(int argc, char *argv[]) {
 	std::cout<<"*5 hours pass*"<<std::endl;
 		
 
+	std::cout<<printMoney<<std::endl;
+}
+
+
+string printMoney(int in_money){
+	(void)in_money;
+	return "";
 }
